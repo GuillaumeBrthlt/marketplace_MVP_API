8c84f18fb82239ffaa4ab03f9470cb1f46c71963
xko0 <germain.rgb@hotmail.fr>
2022-11-29 09:11:36 +0100

    added address to properties

---
+++ db/migrate/20221129080601_add_adress_to_properties.rb
@@ -0,0 +1,7 @@
+class AddAdressToProperties < ActiveRecord::Migration[7.0]
+  def change
+    add_column :properties, :address, :string
+    add_column :properties, :city, :string
+    add_column :properties, :zipcode, :integer
+  end
+end
